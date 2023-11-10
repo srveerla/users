3127dee39923eca846b96edace7d9b3d8929cfb0
srveerla <39939637+srveerla@users.noreply.github.com>
2023-11-10 19:23:54 +0530

    Create users.py

---
+++ users.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({"id": user.id, "username": user.username, "email": user.email})
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(port=5000)
