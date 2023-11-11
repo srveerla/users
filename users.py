82d273b6bf22e0b6b4c95115df6cce8c4a6c7261
srveerla <39939637+srveerla@users.noreply.github.com>
2023-11-11 16:09:09 +0530

    Update users.py

--- users.py
+++ users.py
@@ -1,17 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/users_db'
+app.config["MONGO_URI"] = "mongodb://mongo:27017/library"
 mongo = PyMongo(app)
 
-@app.route('/users/<username>', methods=['GET'])
-def get_user(username):
-    user = mongo.db.users.find_one({'username': username})
-    if user:
-        return jsonify({"username": user['username'], "email": user['email']})
-    else:
-        return jsonify({"error": "User not found"}), 404
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = list(mongo.db.users.find({}, {'_id': 0}))
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    user = User(data['username'], data['email'])
+    mongo.db.users.insert_one(user.__dict__)
+    return 'User added', 201
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0')
